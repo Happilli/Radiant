c845942bbe88a3b9b4aabac68e11851a4625c3cf
Happilli <133653188+Happilli@users.noreply.github.com>
2023-09-01 12:16:50 +0000

    uptimerobotShow

--- webserver.py
+++ webserver.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Radiant is active!"
+    return "Radiant is currently active!"
 
 def run():
     app.run(host="0.0.0.0", port=8080)
